24a1baf06a394ff12808a889f2ea381fa4f15179
GlauberBispo <glauber.bispo@gmail.com>
2022-08-04 20:27:15 -0300

    Entrega exercicio3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,25 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt(`Escreva aqui sua nacionalidade entre as seguintes opções:
+brasileira;
+argentina;
+uruguaia;
+chilena;
+colombiana;
+Outra.`).toLowerCase() //= adicione o prompt aqui
+
+if (nacionalidade === 'brasileira') {
+    alert(`Sua nacionalidade é: ${nacionalidade[0].toUpperCase()}${nacionalidade.substring(1)}`)
+    
+} else if (nacionalidade === 'argentina') {
+    alert(`Sua nacionalidade é: ${nacionalidade[0].toUpperCase()}${nacionalidade.substring(1)}`)
+} else if (nacionalidade === 'uruguaia') {
+    alert(`Sua nacionalidade é: ${nacionalidade[0].toUpperCase()}${nacionalidade.substring(1)}`)
+
+} else if (nacionalidade === 'chilena') {
+    alert(`Sua nacionalidade é: ${nacionalidade[0].toUpperCase()}${nacionalidade.substring(1)}`)
+
+} else if (nacionalidade === 'colombiana') {
+    alert(`Sua nacionalidade é: ${nacionalidade[0].toUpperCase()}${nacionalidade.substring(1)}`)
+
+} else {
+    alert('Você não é de nenhuma das nacionalidades listadas.')
+};
